1844a66297df5d3b8b72779afab9e0e4c017a888
louieogrady <l.ogrady@gmail.com>
2019-08-11 15:31:40 +0100

    prices organised into arrays ready for chart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
 import Tone from "tone";
+import Chart from "./Chart.js";
 
 class App extends Component {
   state = {
+    priceData: [{
+      ethereum: [],
+      bitcoin: []
+    }],
     bitcoin: null,
     ethereum: null
   };
 
+  bitcoinPrice = this.state.priceData[0].bitcoin
+
+  autoFilter = new Tone.AutoFilter("4n").toMaster().start();
   // create synths
-  bitcoinSynth = new Tone.AMSynth().toMaster();
+  bitcoinSynth = new Tone.FMSynth({
+    harmonicity: 10,
+    detune: 0,
+    envelope: {
+      attack: 0.01,
+      decay: 0.01,
+      sustain: 1,
+      release: 0.5
+    },
+    modulationEnvelope: {
+      attack: 1,
+      decay: 0,
+      sustain: 2,
+      release: 0.5
+    }
+  }).connect(this.autoFilter).toMaster();
+
+
+
+
+  ethSynth = new Tone.MembraneSynth().toMaster();
 
   // coincap websockets
   pricesWs = "wss://ws.coincap.io/prices?assets=bitcoin";
-
-
-
 
   getPrices = () => {
     this.connection = new WebSocket(
@@ -28,52 +53,87 @@ class App extends Component {
       console.log(JSON.parse(e.data));
       const data = JSON.parse(e.data);
 
-      if (data.bitcoin) {
-        console.log(data.bitcoin);
-        this.setState({
-          bitcoin: data.bitcoin
-        });
-      } else {
-        console.log('no bitcoin price update')
-      }
+      // this.setState({
+      //   priceData: data
+      // });
 
-      if (data.ethereum) {
+      // if (data.bitcoin) {
+      //   console.log(data.bitcoin);
+      //   this.setState({
+      //     bitcoin: data.bitcoin
+      //   });
+      // } else {
+      //   console.log("no bitcoin price update");
+      // }
+
+      if (data.ethereum && data.bitcoin) {
         console.log(data.ethereum);
-        this.setState({
-          ethereum: data.ethereum
-        });
-      } else {
-        console.log('no ethereum price update')
-      }
-    };
-  };
+        // this.setState({
+        //   ethereum: data.ethereum
+        // });
+
+        this.setState({ priceData: [{
+          ethereum: [...this.state.priceData[0].ethereum, data.ethereum],
+          bitcoin: [...this.state.priceData[0].bitcoin, data.bitcoin]
+        }]
+      })
+    } else if (data.bitcoin) {
+      this.setState({ priceData: [{
+        ethereum: [...this.state.priceData[0].ethereum],
+        bitcoin: [...this.state.priceData[0].bitcoin, data.bitcoin]
+      }]
+    })
+  } else if (data.ethereum) {
+    this.setState({priceData: [{
+        ethereum: [...this.state.priceData[0].ethereum, data.ethereum],
+        bitcoin: [...this.state.priceData[0].bitcoin]
+      }]
+
+    })}
+  }
+}
 
   componentDidMount() {
     this.getPrices();
 
-    setInterval(() => {
-      this.bitcoinSynth.frequency.value = this.state.bitcoin / 25; // bringing down the frequency to something more manageable
-      console.log(this.bitcoinSynth.frequency.value);
-    }, 1000);                                                      // osc frequency is updated every second
 
+    setInterval(() => {
+      let bitcoinPrice = this.state.priceData[0].bitcoin
+    this.bitcoinSynth.frequency.value = bitcoinPrice[bitcoinPrice.length-1] / 25; // selects last item in array, bringing down the frequency to something more manageable
+      console.log(this.bitcoinSynth.frequency.value);
+    }, 1000); // osc frequency is updated every second
   }
 
+  randomTriggerInterval = () => Math.floor(Math.random() * 15000) + 1000; // random generates number between 1 and 15
+
   handleClick = () => {
-    this.bitcoinSynth.frequency.value = this.state.bitcoin / 25;
+    let bitcoinPrice = this.state.priceData[0].bitcoin
+    this.bitcoinSynth.frequency.value = bitcoinPrice[bitcoinPrice.length-1]
     this.bitcoinSynth.triggerAttack();
     console.log("synth started");
+
+    setInterval(() => {
+      // had to put this here rather than componentDidMount
+      this.ethSynth.triggerAttack(this.state.priceData[0].ethereum / 8);
+    }, this.randomTriggerInterval()); // MembraneSynth is triggered by the amount of seconds determined by the random Interval generator
   };
 
   render() {
+    let bitcoinPrice = this.state.priceData[0].bitcoin
+    let ethPrice = this.state.priceData[0].ethereum
     return (
       <div className="App">
-        <h1> bitcoin: {this.state.bitcoin} </h1>
-        <h1> ethereum: {this.state.ethereum} </h1>
+        <h1> bitcoin: {bitcoinPrice[bitcoinPrice.length-1]} </h1>
+        <h1> ethereum: {ethPrice[ethPrice.length-1]} </h1>
 
         <button onClick={this.handleClick}> Start Synth </button>
+
+
       </div>
+
     );
   }
 }
-
 export default App;
+
+// <div className="Chart-Container" style= {{ height: '10em', width: '10em' }}> <Chart priceData={this.state.priceData} />
